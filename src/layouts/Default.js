5615a6ea30275dfc4acdcf262a42c2d78c4d6d83
Whaley <dustindowell22@gmail.com>
2021-07-11 05:58:56 -0500

    Cleanup default layout

--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -1,6 +1,4 @@
 
-// import cc from 'classcat'
-
 import Header from 'components/Header'
 import Panel from 'components/Panel'
 import Sidebar from 'components/Sidebar'
@@ -8,11 +6,6 @@ import Sidebar from 'components/Sidebar'
 import { dispatch } from 'app'
 
 export default (props, children) => state => {
-  // const classList = cc([
-  //   'layout-default',
-  //   state.common.banner && '-banner'
-  // ])
-
   return (
     <div class='layout-default'>
       <Header/>
@@ -23,16 +16,6 @@ export default (props, children) => state => {
       </div>
       <Panel/>
       <Sidebar/>
-      {/* <div class='layout-main-content'>
-        <div class='layout-main-head'>
-          <h1>{props.title}</h1>
-          <div class='component-layout-corner'>
-            <div>
-              <button alt='Menu' onclick={toggle}>|||</button>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   )
 }
