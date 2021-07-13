7beb9f713f82bc8075c50725e036e248909a6bf9
Whaley <dustindowell22@gmail.com>
2021-07-13 03:11:39 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,49 +3,200 @@ export default [
   {
     uid: '14f866ffbe90',
     title: 'Inflatable Avocado Pool Floatie with Ball',
-    price: '$29.99',
+    price: '$39.99',
     amazon: 'https://www.amazon.com/dp/B07LG9RJT7/',
-    tags: ['pool', 'beach'],
+    tags: ['pool', 'beach', 'green'],
     images: [
       '/products/61RKbylA+9L._AC_SL1500_.jpg',
+      '/products/717tdmdQv4L._AC_SL1500_.jpg',
       '/products/61NH8p0QisL._AC_SL1500_.jpg',
-      '/products/61DXBWnDI+L._AC_SL1500_.jpg',
       '/products/71uPv9tJJyL._AC_SL1500_.jpg',
       '/products/619ghvc8ELL._AC_SL1500_.jpg',
       '/products/713Ts8pJHCL._AC_SL1500_.jpg',
-      '/products/717tdmdQv4L._AC_SL1500_.jpg'
+      '/products/61DXBWnDI+L._AC_SL1500_.jpg'
     ]
   },
   {
     uid: '4d76f984e427',
-    title: 'Inflatable Avocado Pool Floatie with Ball',
-    price: '$29.99',
-    amazon: 'https://www.amazon.com/dp/B07LG9RJT7/',
-    tags: ['pool', 'beach'],
+    title: 'Giant Inflatable Unicorn Pool Floatie',
+    price: '$39.99',
+    amazon: 'https://www.amazon.com/dp/B07BMM7JY8/',
+    tags: ['pool', 'beach', 'white'],
     images: [
-      '/products/61RKbylA+9L._AC_SL1500_.jpg',
-      '/products/61NH8p0QisL._AC_SL1500_.jpg',
-      '/products/61DXBWnDI+L._AC_SL1500_.jpg',
-      '/products/71uPv9tJJyL._AC_SL1500_.jpg',
-      '/products/619ghvc8ELL._AC_SL1500_.jpg',
-      '/products/713Ts8pJHCL._AC_SL1500_.jpg',
-      '/products/717tdmdQv4L._AC_SL1500_.jpg'
+      '/products/81ERroLL9HL._AC_SL1500_.jpg',
+      '/products/81WCjnHLwnL._AC_SL1500_.jpg',
+      '/products/81c+On1iMHL._AC_SL1500_.jpg',
+      '/products/81Z8mI6nuOL._AC_SL1500_.jpg',
+      '/products/81bqJhpuKcL._AC_SL1500_.jpg',
+      '/products/61oZ4VrQwlL._AC_SL1364_.jpg'
     ]
   },
   {
     uid: '7985168cfbeb',
-    title: 'Inflatable Avocado Pool Floatie with Ball',
-    price: '$29.99',
-    amazon: 'https://www.amazon.com/dp/B07LG9RJT7/',
-    tags: ['pool', 'beach'],
+    title: 'Inflatable Pineapple Pool Floatie',
+    price: '$39.99',
+    amazon: 'https://www.amazon.com/dp/B01N7KS80U/',
+    tags: ['pool', 'beach', 'yellow'],
     images: [
-      '/products/61RKbylA+9L._AC_SL1500_.jpg',
-      '/products/61NH8p0QisL._AC_SL1500_.jpg',
-      '/products/61DXBWnDI+L._AC_SL1500_.jpg',
-      '/products/71uPv9tJJyL._AC_SL1500_.jpg',
-      '/products/619ghvc8ELL._AC_SL1500_.jpg',
-      '/products/713Ts8pJHCL._AC_SL1500_.jpg',
-      '/products/717tdmdQv4L._AC_SL1500_.jpg'
+      '/products/713oQhc14+L._AC_SL1500_.jpg',
+      '/products/719fjquNffL._AC_SL1344_.jpg',
+      '/products/71TglL8liAL._AC_SL1259_.jpg'
+    ]
+  },
+  {
+    uid: 'b9e9d215ff0d',
+    title: 'Giant Inflatable Flamingo Pool Floatie',
+    price: '$39.99',
+    amazon: 'https://www.amazon.com/dp/B07D1CMSZV/',
+    tags: ['pool', 'beach', 'pink'],
+    images: [
+      '/products/71AU0RSciML._AC_SL1500_.jpg',
+      '/products/71bSEsCsQqL._AC_SL1500_.jpg',
+      '/products/71X4E6Of13L._AC_SL1500_.jpg',
+      '/products/71j6wt72MOL._AC_SL1500_.jpg',
+      '/products/71w+KpBtrHL._AC_SL1500_.jpg',
+      '/products/71IJpuEsmJL._AC_SL1500_.jpg',
+      '/products/7105z9w4YxL._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: 'bd94307d8948',
+    title: '(Pink) SOMIC G951s Pink Stereo Gaming Headset',
+    price: '$49.99',
+    amazon: 'https://www.amazon.com/dp/B07JZ2Z588/',
+    tags: ['computer', 'gaming', 'electronics', 'pink'],
+    images: [
+      '/products/71j0MsxGrjL._AC_SL1400_.jpg'
+    ]
+  },
+  {
+    uid: 'd2a8badf3a0f',
+    title: '(Black) SOMIC G951s Pink Stereo Gaming Headset',
+    price: '$49.99',
+    amazon: 'https://www.amazon.com/dp/B08K8VK42X/',
+    tags: ['computer', 'gaming', 'electronics', 'black'],
+    images: [
+      '/products/71ZUHBbRQEL._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: '361b07a77fcf',
+    title: '(Purple) SOMIC G951s Pink Stereo Gaming Headset',
+    price: '$49.99',
+    amazon: 'https://www.amazon.com/dp/B07Q7GQT72/',
+    tags: ['computer', 'gaming', 'electronics', 'purple'],
+    images: [
+      '/products/71g6+Qt2PUL._AC_SL1400_.jpg'
+    ]
+  },
+  {
+    uid: '310620cf7402',
+    title: '(Pink) Silicone PS4 Controller Skin for PS4',
+    price: '$19.99',
+    amazon: 'https://www.amazon.com/dp/B07MSG1C5C/',
+    tags: ['console', 'gaming', 'electronics', 'pink'],
+    images: [
+      '/products/71-k5MlkvYL._AC_SL1500_.jpg',
+      '/products/714342jlNrL._AC_SL1500_.jpg',
+      '/products/81WnHAECBzL._AC_SL1500_.jpg',
+      '/products/71jgwMYg3-L._AC_SL1500_.jpg',
+      '/products/71oP9Ma5q6L._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: '2569a55c77da',
+    title: '(Blue) Silicone PS4 Controller Skin for PS4',
+    price: '$19.99',
+    amazon: 'https://www.amazon.com/dp/B07WPY1BN9/',
+    tags: ['console', 'gaming', 'electronics', 'blue'],
+    images: [
+      '/products/61VxQitv5RL._AC_SL1500_.jpg',
+      '/products/61yRWYWqCyL._AC_SL1500_.jpg',
+      '/products/613qCu1FF-L._AC_SL1500_.jpg',
+      '/products/61GwLfBOksL._AC_SL1500_.jpg',
+      '/products/61crNgZqEuL._AC_SL1500_.jpg',
+      '/products/611gOpSXaJL._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: '30f62e272454',
+    title: '(Purple) Silicone PS4 Controller Skin for PS4',
+    price: '$19.99',
+    amazon: 'https://www.amazon.com/dp/B07X2M84K5/',
+    tags: ['console', 'gaming', 'electronics', 'purple'],
+    images: [
+      '/products/61xCC7fDWAL._AC_SL1500_.jpg',
+      '/products/61esrEWsaSL._AC_SL1500_.jpg',
+      '/products/61zIMd5-caL._AC_SL1500_.jpg',
+      '/products/71Bv1bRpePL._AC_SL1500_.jpg',
+      '/products/61J1Uq8BaBL._AC_SL1500_.jpg',
+      '/products/71xKSPDXDKL._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    active: false,
+    uid: '147f9c1253b9',
+    title: '(Sakura Pink) Silicone PS4 Controller Skin for PS4',
+    price: '$19.99',
+    amazon: 'https://www.amazon.com/dp/B098317CPN/',
+    tags: ['console', 'gaming', 'electronics', 'pink', 'sakura'],
+    images: [
+      '/products/61hIdPpL5yS._AC_SL1500_.jpg',
+      '/products/71T3EeURkCS._AC_SL1500_.jpg',
+      '/products/71vwug5XlmS._AC_SL1500_.jpg',
+      '/products/71rk2MVZAxS._AC_SL1500_.jpg',
+      '/products/71i2oDSL75S._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: 'bcc3a660b7c3',
+    title: '(Black) Havit Mechanical Keyboard Wired 89 Keys Gaming Keyboard Red Switch Keyboard with PBT Keycaps',
+    price: '$49.99',
+    amazon: 'https://www.amazon.com/dp/B085ZDXGZW/',
+    tags: ['computer', 'gaming', 'electronics', 'black', 'orange'],
+    images: [
+      '/products/61Om-iwnAbL._AC_SL1500_.jpg',
+      '/products/71VYjUYHTZL._AC_SL1500_.jpg',
+      '/products/71CYRAHhhYL._AC_SL1500_.jpg',
+      '/products/71X3Nj0cQ0L._AC_SL1500_.jpg',
+      '/products/71qECgPqNwL._AC_SL1500_.jpg',
+      '/products/71v0zvOYerL._AC_SL1500_.jpg',
+      '/products/71qAFOTfuOL._AC_SL1500_.jpg',
+      '/products/610HzEWRKFL._AC_SL1500_.jpg',
+      '/products/71ELCra1c+L._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: 'ee355d26b88b',
+    title: '(White) Havit Mechanical Keyboard Wired 89 Keys Gaming Keyboard Red Switch Keyboard with PBT Keycaps',
+    price: '$49.99',
+    amazon: 'https://www.amazon.com/dp/B08GRZ3VN3/',
+    tags: ['computer', 'gaming', 'electronics', 'white'],
+    images: [
+      '/products/71aznZyX2OL._AC_SL1500_.jpg',
+      '/products/71hBRZgLh+L._AC_SL1500_.jpg',
+      '/products/719LRErIc1L._AC_SL1500_.jpg',
+      '/products/71bFx7Id4yL._AC_SL1500_.jpg',
+      '/products/71iZXc-HkSL._AC_SL1500_.jpg',
+      '/products/81nzoL6FudL._AC_SL1500_.jpg',
+      '/products/71pszbqJlTL._AC_SL1500_.jpg',
+      '/products/71nVGJwk0BL._AC_SL1500_.jpg'
+    ]
+  },
+  {
+    uid: 'dd0e1a0d523c',
+    title: '(White) PICTEK RGB Gaming Mouse Wired',
+    price: '$19.99',
+    amazon: 'https://www.amazon.com/dp/B08ZN16KTV/',
+    tags: ['computer', 'gaming', 'electronics', 'white'],
+    images: [
+      '/products/31dzH4WodjS._AC_.jpg',
+      '/products/61uP4szl2NS._AC_SL1500_.jpg',
+      '/products/71o2OCdXYIS._AC_SL1500_.jpg',
+      '/products/71btlRjGlyS._AC_SL1500_.jpg',
+      '/products/71gF-YhtTtS._AC_SL1500_.jpg',
+      '/products/71rE6oA-u7S._AC_SL1500_.jpg',
+      '/products/71rE6oA-u7S._AC_SL1500_.jpg'
     ]
   }
 ]
