e28e7571110cab40a02c81702031c77d44adaa8b
Whaley <dustindowell22@gmail.com>
2021-07-14 05:22:10 -0500

    Wholesale content

--- src/pages/Wholesale.js
+++ src/pages/Wholesale.js
@@ -1,15 +1,26 @@
 
 import Default from 'layouts/Default'
 import Hero from 'components/Hero'
+import Link from 'ui/Link'
+
+const Item = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <Link to='/'>Link to something</Link>
+    </div>
+  )
+}
 
 const Wholesale = () => {
   return (
     <div class='page-wholesale'>
       <Hero/>
       <div class='page-wholesale-row'>
-        <div>one</div>
-        <div>one</div>
-        <div>one</div>
+        <Item/>
+        <Item/>
+        <Item/>
       </div>
     </div>
   )
